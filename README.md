52a7da46b9626cd8fbba88b40732db1bcfda07eb
chemacenturion <centurion.philadelphia@gmail.com>
2021-06-29 23:53:15 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Homework is designed to take in high scores when taking a coding quiz
+* Designed a container with the various buttons needed
+* Setup a css file to properly style and space the containers
+* Entered in various functions to make quiz run properly
+
+
+
+✅Timer starts when start button is clicked
+✅When a question is answered the score is tallied and the Next button appears
+✅When the question is answered incorrectly time is subtracted from the clock
+
+# Unfulfilled portion
+*Was unable to get game to end when clock runs out
+*Was unable to get intials and score to store together without separate HTML and was told not to code with separate HTML
+✅New high score does store in the inspect elements memory local storage and within the "View High Scores" div.
+
+https://chemacenturion.github.io/Coding-Quiz/
--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,21 @@ var nextBtn = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
-var count = 60;
+var count = 100;
+
+var rankingButton = document.getElementById("ranking-button")
+
+rankingButton.addEventListener('click', showMyHighScore);
+
+function showMyHighScore() {
+    var highScore = localStorage.getItem("newScore");
+    // now put ^ highscore onto the page by appending it where it needs to go.
+    document.getElementById("final-score").textContent = score
+}
+
+
+
+var score = 0;
 
 
 var questions = [
@@ -57,6 +71,7 @@ var questions = [
         ]
     }
 ]
+console.log("count: ", count)
 
 let shuffledQuestions, currentQuestionsIndex
 
@@ -64,23 +79,34 @@ startBtn.addEventListener('click', startGame)
 nextBtn.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
-})
- 
+});
+
+
+
+function saveMyScore() {
+    localStorage.setItem("newScore: ", score);
+    var savedScore = localStorage.getItem("newScore")
+}
+
+
 function startGame() {
     startBtn.classList.add('hide')
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-    var intID = setInterval(countdown, 1000);
+
+
+    //counter
+    intId = setInterval(countdown, 1000);
     var counter = document.querySelector("#time_remaining");
-    
+
     function countdown() {
         count--;
-        counter.textContent = "Time Remaining:" + count;
-        if(count===0) {
-            clearInterval(intID);
-            GameOverScreen();
+        counter.textContent = "Time remaining: " + count;
+        if (count === 0) {
+            clearInterval(intId);
         }
+
     }
+
+
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
@@ -89,7 +115,7 @@ function startGame() {
 //GIVEN I am taking a code quiz
 // WHEN I answer a question
 // THEN I am presented with another question
-function setNextQuestion () {
+function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
@@ -97,7 +123,7 @@ function setNextQuestion () {
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement ('button')
+        const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
         if (answer.correct) {
@@ -108,7 +134,7 @@ function showQuestion(question) {
     })
 }
 
-function resetState () {
+function resetState() {
     clearStatusClass(document.body)
     nextBtn.classList.add('hide')
     while (answerButtonsElement.firstChild) {
@@ -119,34 +145,64 @@ function resetState () {
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
+    if (correct) {
+        console.log("you've selected the correct answer!!")
+        score = score + 5;
+        console.log("score is after selecting the correct answer: ", score)
+
+    } else {
+        count = count - 10;
+        console.log("count is after decreasing, now: ", count)
+        console.log("decreasing score!", score)
+
+    }
+    // setStatusClass(document.body, correct)
+
+    // Array.from(answerButtonsElement.children).forEach(button => {
+    //     // setStatusClass(button, button.dataset.correct);
+
+    //     // if (button.dataset.incorrect) {
+    //     //     count = count - 10;
+    //     //     console.log("now count is: ", count)
+    //     // }
+    // })
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextBtn.classList.remove('hide')
     } else {
-        startBtn.innerText = 'Log Score'
-        startBtn.classList.remove('hide')
+        startBtn.innerText = 'Restart'
+        startBtn.classList.remove('hide');
+        saveMyScore()
+
     }
+
 }
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    }else{
-        element.classList.add('incorrect')
-        count = count-1;
-    }
-}
+// function setStatusClass(element, correct) {
+//     clearStatusClass(element);
+//     console.log("correct is: ", correct)
+//     if (correct) {
+//         element.classList.add('correct')
+//         console.log("nswer is correct");
+//         score = score + 5;
+//         console.log("incraeasing score!", score)
+
+//     } else {
+//         element.classList.add('incorrect');
+//         console.log("nswer is incorrect")
+//         score = score + 5;
+//         console.log("decraeasing score!", score)
+
+
+
+//     }
+// }
 
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('incorrect')
 }
 
-document.querySelector(".rankingButton").addEventListener("click" , function() {
+document.querySelector(".rankingButton").addEventListener("click", function () {
     var ranking = document.querySelector(".ranking");
-ranking.style.display = ranking.style.display === 'none' ? 'block' : 'none';
+    ranking.style.display = ranking.style.display === 'none' ? 'block' : 'none';
 })
